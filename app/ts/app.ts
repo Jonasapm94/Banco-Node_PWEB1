4036cfdbea6ed4e382e668ccba879eed0b865958
Jonas Ariel Passos de Medeiros <20202370008@ad.ifpb.edu.br>
2023-03-24 16:23:11 -0300

    cont.

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -6,7 +6,7 @@ const c1 = new Conta('1', 100);
 const p1 = new Poupanca('2', 100);
 const cb1 = new ContaBonificada('3', 0);
 
-const clientes = new Clientes();
+
 
 
 console.log('Conta: ' + c1.saldo);
@@ -16,3 +16,15 @@ console.log('Poupanca: ' + p1.saldo);
 
 cb1.creditar(100);
 console.log('Conta Bonificada: ' + cb1.saldo);
+
+
+const clientes = new Clientes();
+
+clientes.inserir(new Cliente('Jonas', '012345678901', c1));
+console.log(clientes.listar());
+
+console.log(clientes.pesquisar('012345678901'));
+
+clientes.remover('012345678901');
+console.log(clientes.listar());
+
